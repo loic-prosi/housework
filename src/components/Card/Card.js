55e455545102b25d24e50aedc176a19e205c2ac9
loic-prosi <loicprosi@gmail.com>
2020-05-03 22:24:35 +0200

    <Feature> Added empty card name handling

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,9 +1,15 @@
 import React from "react";
 
 const Card = props => {
+  let { name } = props;
+
+  if (!name || name.length === 0) {
+    name = "???";
+  }
+
   return (
     <div className="card">
-      <p>{props.name}</p>
+      <p>{name}</p>
     </div>
   );
 };
