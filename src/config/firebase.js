a686239e6226e573d0bbaa35e3ff3a992147ab57
loic-prosi <loicprosi@gmail.com>
2020-05-05 01:28:28 +0200

    <Feature> Added config template

---
+++ src/config/firebase.js
@@ -0,0 +1,12 @@
+export const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+
+export const databasePath = "google-sheet-id/data/";
