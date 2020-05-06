eb5b879b9223904988ae3225c6dc96fb19ad50a3
loic-prosi <loicprosi@gmail.com>
2020-05-06 02:19:44 +0200

    <Feature> Added new compute period

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,24 +6,32 @@ const Card = props => {
   let { id, name, week } = props;
   const letter = name[0];
 
-  const startDate = moment().week(week).startOf("isoWeek").format("D MMMM");
+  const currentDay = moment();
+
+  const startDate = moment().week(week).startOf("isoWeek").add(3, "days");
   const endDate = moment()
-    .week(week + 2)
+    .week(week + 1)
     .startOf("isoWeek")
-    .subtract(1, "days")
-    .format("D MMMM");
+    .add(3, "days")
+    .subtract(1, "minutes");
+
+  const currentDayInPeriod = currentDay.isBetween(startDate, endDate);
 
   if (!name || name.length === 0) {
     name = "???";
   }
 
   return (
-    <div className={`card card-${id}`}>
+    <div
+      className={`card card-${id} ${
+        currentDayInPeriod ? "card--current-period" : ""
+      }`}
+    >
       <div className="card__letter">{letter}</div>
       <div className="card__info">
         <h2 className="card__name">{name}</h2>
         <p className="card__period">
-          {startDate} - {endDate}
+          {startDate.format("D MMMM")} - {endDate.format("D MMMM")}
         </p>
       </div>
     </div>
